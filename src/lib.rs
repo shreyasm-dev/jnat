517c7a5c642733cbc71bc0188528afe82f0b994b
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-07-24 05:50:29 +0000

    Create Env struct
    Not useful right now, no functionality implemented, all it does is re-expose a JNIEnv that you pass to it

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-  left + right
+use jni::JNIEnv;
+
+struct Env<'a> {
+  pub jni_env: *mut JNIEnv<'a>, // TODO: Temporarily pub, remove later
 }
 
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
+impl Env<'_> {
+  fn new(jni_env: *mut JNIEnv) -> Env {
+    Env { jni_env }
   }
 }
